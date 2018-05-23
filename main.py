12b98143e6e5c6a3764087774f1f2085f6e5d429
Rane Gridley <ranegri3@gmail.com>
2018-05-23 13:47:07 -0700

    Add First Iteration

---
+++ main.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+# https://github.com/roppert/mosquitto-python-example/blob/master/subscriber.py
+# https://github.com/roppert/mosquitto-python-example/blob/master/publisher.py
+
+import os
+import time
+import socketio
+
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+from gevent import pywsgi
+from random import randint
+from flask import Flask, send_from_directory
+
+# ==========
+# APP
+# ==========
+
+# Configure Socket / App
+sio = socketio.Server(logger=True, async_mode='gevent')
+app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = 'secret!'
+app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
+thread = None
+
+# Test "Device" State (On/Off)
+test_device_on = False
+
+# Socket Config
+mqtt_namespace = '/mqtt'
+
+# MQTT Config
+mqtt_broker_host = "iot.eclipse.org"
+mqtt_topics = [ ["agent/updated", 0], ["athing/#", 0] ]
+
+# ==========
+# MQTT
+# ==========
+
+# MQTT Handlers
+def on_message(mosq, obj, msg):
+    print("{} {} {}".format(msg.topic, msg.qos, msg.payload))
+    sio.emit('new message', {'data': str(msg.payload.decode())},
+                namespace=mqtt_namespace)
+    mosq.publish('pong', 'ack', 0)
+
+def on_publish(mosq, obj, mid):
+    pass
+
+# ==========
+# Socket IO
+# ==========
+
+# Socket Helpers
+def emit_test_device_state():
+    sio.emit('test device status', { 'test_device_on': test_device_on }, namespace=mqtt_namespace)
+
+def start_background_tasks():
+    global thread
+    if thread is None:
+        thread = sio.start_background_task(background_test_thread)
+
+# Socket IO Handlers
+@sio.on('client connected', namespace=mqtt_namespace)
+def connect(sid):
+    emit_test_device_state()
+    print('client connected...', sid)
+
+@sio.on('client disconnected', namespace=mqtt_namespace)
+def disconnect(sid):
+    print('client disconnected...', sid)
+
+
+@sio.on('test state change', namespace=mqtt_namespace)
+def update_test_state(sid, data):
+    global test_device_on
+    test_device_on = data.get('test_device_on', False)
+    emit_test_device_state()
+    print('test data turned {}...'.format('on' if test_device_on else 'off'))
+
+# Test thread
+def background_test_thread():
+    """Periodically publish random 'device' data"""
+    global test_device_on
+    msgs = [{'topic': "agent/updated", 'payload': "agent updated"},
+            {'topic': "athing/led", 'payload': "ON"},
+            {'topic': "athing/status", 'payload': "athing offline"},
+            {'topic': "athing/status", 'payload': "athing online"}]
+    while True:
+        sio.sleep(1)
+        if test_device_on:
+            msg = randint(0, len(msgs) - 1)
+            publish.single(hostname=mqtt_broker_host, **msgs[msg])
+
+# ==========
+# Flask
+# ==========
+
+# Flask Route Handlers
+@app.route('/')
+def index():
+    start_background_tasks()
+    return app.send_static_file('index.html')
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+
+if __name__ == '__main__':
+
+    # Create Mqtt client and connect
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_message = on_message
+    mqtt_client.on_publish = on_publish
+
+    # mqtt_client.tls_set('root.ca', certfile='c1.crt', keyfile='c1.key')
+    mqtt_client.connect(mqtt_broker_host, 1883, 60)
+
+    # Subscribe to our topics
+    for topic in mqtt_topics:
+        mqtt_client.subscribe(*topic)
+
+    # Start listening
+    mqtt_client.loop_start()
+
+    # Deploy with gevent - gevent allows for cross context emit events
+    try:
+        from geventwebsocket.handler import WebSocketHandler
+        websocket = True
+    except ImportError:
+        websocket = False
+    if websocket:
+        pywsgi.WSGIServer(('', 8080), app,
+                            handler_class=WebSocketHandler).serve_forever()
+    else:
+        pywsgi.WSGIServer(('', 8080), app).serve_forever()
---
+++ static/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+  
+  // Global / Init Variables
+  const namespace = '/mqtt';
+  const socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
+
+  // $Element Variables
+  const $messages = $( "#messages" )
+  const $testSocketButton = $('#test-control');
+  const $clearMessagesButton = $('#clear-messages');
+
+  // State Variables
+  let testDeviceOn = undefined
+  let hideTestDeviceMessages = false
+  
+  // Add new message
+  const addMessage = (message) => {
+    $messages.prepend( "<p>" + message + "</p>" );
+  }
+
+  // Add test message if we are allowed
+  const addTestingMessage = (message) => {
+    if (!hideTestDeviceMessages && testDeviceOn) {
+      addMessage(message);
+    }
+  }
+
+  // Toggle Device Test State
+  setDeviceTestingState = (test_device_on) => {
+    testDeviceOn = test_device_on
+    $testSocketButton.html(testDeviceOn ? 'Stop Test Device' : 'Start Test Device')
+  }
+  
+  const toggleTestDevice = () => {
+    // Must be boolean type
+    if (typeof(testDeviceOn) === typeof(true)) {
+      socket.emit('test state change', {test_device_on: !testDeviceOn});
+    }
+  }
+
+  // Keyboard events
+  
+  // Click events
+  
+  $clearMessagesButton.click(() => {
+    $messages.empty();
+  });
+  
+  $testSocketButton.click(() => {
+      toggleTestDevice()
+  });
+
+  // Socket events
+  
+  socket.on('new message', (message) => {
+    addTestingMessage(message.data);
+  });
+
+  socket.on('test device status', (message) => {
+    console.log('test device is on: ', message)
+    setDeviceTestingState(message.test_device_on)
+  });
+
+  socket.on('connect', function(data) {
+    socket.emit('client connected');
+    console.log('socket connected...')
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('socket disconnected...');
+  });
+  
+  socket.on('reconnect', () => {
+    socket.emit('client connected');
+    console.log('socket reconnecting...');
+  });
+  
+  socket.on('reconnect_error', () => {
+    console.log('attempt to reconnect has failed...');
+  });
+  
+});
